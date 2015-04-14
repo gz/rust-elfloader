53d03bc6cb6e88d00b7de986ea0ecbc72431d33b
Gerd Zellweger <mail@gerdzellweger.com>
2015-04-14 21:31:35 +0200

    Factor methods into ElfBinary struct impl.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(custom_derive)]
 #![feature(no_std)]
 #![feature(core)]
 #![no_std]
@@ -14,51 +15,76 @@ extern crate klogger;
 #[macro_use]
 extern crate std;
 
-mod types;
+mod elf;
+use core::prelude::*;
 use core::mem::{transmute, size_of};
+use core::slice;
 
+/// Abstract representation of a loadable ELF binary.
 pub struct ElfBinary {
-    pub start: u64,
-    pub size: usize,
+    name: &'static str,
+    region: &'static [u8],
+    header: &'static elf::FileHeader,
 }
 
-pub fn parse_elf(binary: &'static [u8]) {
+/// Verify that memory region starts with a correct ELF magic.
+fn valid_elf_magic(region: &'static [u8]) -> bool {
+    region[0] == elf::ELFMAG0 &&
+    region[1] == elf::ELFMAG1 &&
+    region[2] == elf::ELFMAG2 &&
+    region[3] == elf::ELFMAG3
+}
 
-    // Verify the magic number
-    if binary[0] != types::ELFMAG0 ||
-       binary[1] != types::ELFMAG1 ||
-       binary[2] != types::ELFMAG2 ||
-       binary[3] != types::ELFMAG3 {
-        return
+impl ElfBinary {
+
+    /// Create a new ElfBinary.
+    /// Makes sure that the provided region has valid ELF magic byte sequence
+    /// and is big enough to contain at least the ELF file header
+    /// otherwise it will return None.
+    pub fn new(name: &'static str, region: &'static [u8]) -> Option<ElfBinary> {
+        if region.len() >= size_of::<elf::FileHeader>() && valid_elf_magic(region) {
+            let header: &elf::FileHeader = unsafe { transmute(&region[0]) };
+            return Some(ElfBinary { name: name, region: region, header: header });
+        }
+
+        None
     }
 
-    //log!("header size = {}", size_of::<types::FileHeader>() );
-    let header: &types::FileHeader = unsafe { transmute(&binary[0]) };
-    log!("header = {}", header);
-    log!("Start of program header: {}", header.phoff);
-    log!("Start of section header: {}", header.shoff);
+    /// Print the program header.
+    pub fn print_headers(&self) {
+        for p in self.program_headers() {
+            log!("pheader = {}", p);
+        }
+    }
 
-    let correct_class = header.ident.class == types::ELFCLASS64;
-    let correct_elfversion = header.ident.version == types::EV_CURRENT;
-    let correct_data = header.ident.data == types::ELFDATA2LSB;
-    let correct_osabi = header.ident.osabi == types::ELFOSABI_SYSV || header.ident.osabi == types::ELFOSABI_LINUX;
-    let correct_type = header.elftype == types::ET_EXEC || header.elftype == types::ET_DYN;
-    let correct_machine = header.machine == types::EM_X86_64;
+    /// Create a slice of the program headers.
+    fn program_headers(&self) -> &[elf::ProgramHeader] {
+        let correct_header_size = self.header.phentsize as usize == size_of::<elf::ProgramHeader>();
+        let pheader_region_size = self.header.phoff as usize + self.header.phnum as usize * self.header.phentsize as usize;
+        let big_enough_region = self.region.len() >= pheader_region_size;
 
-    if !correct_class ||
-       !correct_data ||
-       !correct_elfversion ||
-       !correct_machine ||
-       !correct_osabi ||
-       !correct_type {
-        log!("Unable to load this ELF file.");
-        return
-       }
+        if !correct_header_size || !big_enough_region {
+            return &[];
+        }
 
+        let pheaders: &[elf::ProgramHeader] = unsafe {
+            core::slice::from_raw_parts(
+                transmute(&self.region[self.header.phoff as usize]),
+                self.header.phnum as usize)
+        };
+        return pheaders;
+    }
 
-    log!("sizeof(ProgramHeader) = {}", size_of::<types::ProgramHeader>());
-    let pheader: &types::ProgramHeader = unsafe { transmute(&binary[header.phoff as usize]) };
+    /// Can we load the binary on our platform?
+    fn can_load(&self) -> bool {
+        let correct_class = self.header.ident.class == elf::ELFCLASS64;
+        let correct_elfversion = self.header.ident.version == elf::EV_CURRENT;
+        let correct_data = self.header.ident.data == elf::ELFDATA2LSB;
+        let correct_osabi = self.header.ident.osabi == elf::ELFOSABI_SYSV || self.header.ident.osabi == elf::ELFOSABI_LINUX;
+        let correct_type = self.header.elftype == elf::ET_EXEC || self.header.elftype == elf::ET_DYN;
+        let correct_machine = self.header.machine == elf::EM_X86_64;
 
-    log!("pheader = {}", pheader);
+        correct_class && correct_data && correct_elfversion && correct_machine && correct_osabi && correct_type
+    }
 
 }
