f2f1989150ef1fb1c31f634f8c8f4c169a8714b4
landhb <landhb@users.noreply.github.com>
2022-03-01 14:08:49 -0500

    Adding 32bit tests

--- src/test.rs
+++ src/test.rs
@@ -46,8 +46,9 @@ mod test {
         }
 
         fn relocate(&mut self, entry: RelocationEntry) -> Result<(), ElfLoaderErr> {
+            use crate::arch::x86::RelocationTypes::*;
             use crate::arch::x86_64::RelocationTypes::*;
-            use RelocationType::x86_64;
+            use RelocationType::{x86, x86_64};
 
             // Get the pointer to where the relocation happens in the
             // memory where we loaded the headers
@@ -60,6 +61,21 @@ mod test {
             let addr: *mut u64 = (self.vbase + entry.offset) as *mut u64;
 
             match entry.rtype {
+                // x86
+                x86(R_386_32) => Ok(()),
+                x86(R_386_RELATIVE) => {
+                    info!("R_RELATIVE {:p} ", addr);
+                    self.actions
+                        .push(LoaderAction::Relocate(addr as u64, self.vbase));
+                    Ok(())
+                }
+                x86(R_386_GLOB_DAT) => {
+                    trace!("R_386_GLOB_DAT: Can't handle that.");
+                    Ok(())
+                }
+                x86(R_386_NONE) => Ok(()),
+
+                // x86_64
                 x86_64(R_AMD64_64) => {
                     trace!("R_64");
                     Ok(())
@@ -78,7 +94,7 @@ mod test {
                     Ok(())
                 }
                 x86_64(R_AMD64_GLOB_DAT) => {
-                    trace!("TypeRela64::R_GLOB_DAT: Can't handle that.");
+                    trace!("R_AMD64_GLOB_DAT: Can't handle that.");
                     Ok(())
                 }
                 x86_64(R_AMD64_NONE) => Ok(()),
@@ -162,14 +178,101 @@ mod test {
             .iter()
             .find(|&&x| x == LoaderAction::Relocate(0x1000_0000 + 0x200dc0, 0x1000_0000 + 0x000600))
             .is_some());
+    }
 
-        //info!("test {:#?}", loader.actions);
+    #[test]
+    fn load_pie_elf_32() {
+        init();
+        let binary_blob = fs::read("test/test32").expect("Can't read binary");
+        let binary = ElfBinary::new(binary_blob.as_slice()).expect("Got proper ELF file");
+
+        assert!(binary.is_pie());
+
+        let mut loader = TestLoader::new(0x1000_0000);
+        binary.load(&mut loader).expect("Can't load?");
+
+        for action in loader.actions.iter() {
+            println!("{:?}", action);
+        }
+
+        // View allocate/load actions with readelf -l [binary]
+        // Type           Offset   VirtAddr   PhysAddr   FileSiz MemSiz  Flg Align
+        // LOAD           0x000000 0x00000000 0x00000000 0x003bc 0x003bc R   0x1000
+        // LOAD           0x001000 0x00001000 0x00001000 0x00288 0x00288 R E 0x1000
+        // LOAD           0x002000 0x00002000 0x00002000 0x0016c 0x0016c R   0x1000
+        // LOAD           0x002ef4 0x00003ef4 0x00003ef4 0x00128 0x0012c RW  0x1000
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Allocate(VAddr::from(0x0u64), 0x003bc, Flags(4)))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Allocate(VAddr::from(0x1000u64), 0x288, Flags(1 | 4)))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Allocate(VAddr::from(0x002000u64), 0x0016c, Flags(4)))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Allocate(VAddr::from(0x3ef4u64), 0x12c, Flags(2 | 4)))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Load(VAddr::from(0x0u64), 0x003bc))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Load(VAddr::from(0x001000u64), 0x00288))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Load(VAddr::from(0x002000u64), 0x0016c))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Load(VAddr::from(0x00003ef4u64), 0x00128))
+            .is_some());
+
+        // View relocation actions with readelf -r [binary]
+        // Offset     Info    Type            Sym.Value  Sym. Name
+        // 00003ef4  00000008 R_386_RELATIVE
+        // 00003ef8  00000008 R_386_RELATIVE
+        // 00003ff8  00000008 R_386_RELATIVE
+        // 00004018  00000008 R_386_RELATIVE
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Relocate(0x1000_0000 + 0x00003ef4, 0x1000_0000))
+            .is_some());
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Relocate(0x1000_0000 + 0x00003ef8, 0x1000_0000))
+            .is_some());
     }
 
     #[test]
     fn check_nopie() {
         init();
         let binary_blob = fs::read("test/test_nopie").expect("Can't read binary");
+        let binary = ElfBinary::new(binary_blob.as_slice()).expect("Got proper ELF file");
+
+        assert!(!binary.is_pie());
+    }
+
+    #[test]
+    fn check_nopie_32() {
+        init();
+        let binary_blob = fs::read("test/test32_nopie").expect("Can't read binary");
         let binary = ElfBinary::new(binary_blob.as_slice()).expect("Got proper ELF file");
 
         assert!(!binary.is_pie());
@@ -204,6 +307,38 @@ mod test {
             .actions
             .iter()
             .find(|&&x| x == LoaderAction::Tls(VAddr::from(0x200db4u64), 0x4, 0x8, 0x4))
+            .is_some());
+    }
+
+    #[test]
+    fn check_tls_32() {
+        init();
+
+        let binary_blob = fs::read("test/tls32").expect("Can't read binary");
+        let binary = ElfBinary::new(binary_blob.as_slice()).expect("Got proper ELF file");
+        let mut loader = TestLoader::new(0x1000_0000);
+        binary.load(&mut loader).expect("Can't load?");
+        /*
+        TLS produces entries of this form:
+        pheader = Program header:
+        type:             Ok(Tls)
+        flags:              R
+        offset:           0x2ef0
+        virtual address:  0x3ef0
+        physical address: 0x3ef0
+        file size:        0x4
+        memory size:      0x8
+        align:            0x4
+
+        File size is 0x4 because we have one tdata entry; memory size
+        is 8 because we also have one bss entry that needs to be written with zeroes.
+        So to initialize TLS: we allocate zeroed memory of size `memory size`, then copy
+        file size starting at virtual address in the beginning.
+        */
+        assert!(loader
+            .actions
+            .iter()
+            .find(|&&x| x == LoaderAction::Tls(VAddr::from(0x3ef0u64), 0x4, 0x8, 0x4))
             .is_some());
     }
 }
